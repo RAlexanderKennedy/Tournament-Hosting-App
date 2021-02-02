e630b99e98dde9ce6e9c720ff05c92e4253c54a0
Blake Washington <blakewash1@gmail.com>
2021-02-02 14:17:01 -0500

    started on VueJs connectivity

--- vue/src/components/MainFeed.vue
+++ vue/src/components/MainFeed.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <div v-for="tournament in tournaments" v-bind:key="tournament.id">
+      {{tournament.name}}
+    </div>
+  </div>
 </template>
 
 <script>
+import tournamentService from "@/services/TournamentService.js";
+
 export default {
+  name: "main-feed",
+  data() {
+    return {
+      tournaments: []
+    }
+  },
+  created() {
+    tournamentService.getTournaments().then(response => {
+      this.tournaments = response.data;
+    });
+  },
+  methods: {
+    
+  }
 
 }
 </script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <MainFeed />
   </div>
 </template>
 
 <script>
+import MainFeed from "@/components/MainFeed";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    MainFeed
+  }
 };
 </script>
