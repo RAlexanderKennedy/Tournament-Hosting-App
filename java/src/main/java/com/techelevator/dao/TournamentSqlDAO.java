ca06a15c10b67f54c4afc2214d9e7a9189ab9a58
Alex Kennedy <rakennedyj18@gmail.com>
2021-02-02 10:01:52 -0500

    Completed tentative TournamentDAO

--- java/src/main/java/com/techelevator/dao/TournamentSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/TournamentSqlDAO.java
@@ -18,7 +18,7 @@ public class TournamentSqlDAO implements TournamentDAO {
 	public TournamentSqlDAO(DataSource datasource) {
 		template = new JdbcTemplate(datasource);
 	}
-	//TODO confirm table/column names are correct
+	
 	@Override
 	public List<Tournament> getAllTournaments() {
 		String sql = "select * from tournaments";
@@ -27,16 +27,15 @@ public class TournamentSqlDAO implements TournamentDAO {
 		SqlRowSet result = template.queryForRowSet(sql);
 		
 		while (result.next()) {
-			String name = result.getString("name");
-			int id = result.getInt("id");
-			//TODO break users into an array later on 
-			//(will need to check how data is displayed on the table)
-			String users = result.getString("users");
+			String name = result.getString("tournament_name");
+			int id = result.getInt("tournament_id");
+			//TODO remove?
+			//String users = result.getString("users");
 			String status = result.getString("status");
-			//TODO get host data by id and instantiate a user object
 			int hostId = result.getInt("host_id");
-			//TODO finish instantiating tournament with values from above
-			Tournament tournament = new Tournament(id, status);
+			String time = result.getString("tournament_time");
+			String date = result.getString("tournament_date");
+			Tournament tournament = new Tournament(name, time, date, id, hostId, status);
 			tournaments.add(tournament);
 		}
 		
@@ -45,46 +44,45 @@ public class TournamentSqlDAO implements TournamentDAO {
 
 	@Override
 	public Tournament getTournamentById(int searchId) {
-		String sql = "select * from tournaments where id = ?";
+		String sql = "select * from tournaments where tournament_id = ?";
 		SqlRowSet result = template.queryForRowSet(sql, searchId);
 		
 		while (result.next()) {
-			String name = result.getString("name");
-			int id = result.getInt("id");
-			//TODO break users into an array later on 
-			//(will need to check how data is displayed on the table)
-			String users = result.getString("users");
+			String name = result.getString("tournament_name");
+			int id = result.getInt("tournament_id");
+			//TODO remove?
+			//String users = result.getString("users");
 			String status = result.getString("status");
-			//TODO get host data by id and instantiate a user object
 			int hostId = result.getInt("host_id");
-			//TODO finish instantiating tournament with values from above
-			Tournament tournament = new Tournament(id, status);
+			String time = result.getString("tournament_time");
+			String date = result.getString("tournament_date");
+			Tournament tournament = new Tournament(name, time, date, id, hostId, status);
 			return tournament;
 		}
-		
+		return null;
 	}
 
 	@Override
 	public void createTournament(Tournament newTournament) {
 		String name = newTournament.getName();
-		int id = newTournament.getId();
-		User[] users = newTournament.getListOfUsers();
 		String status = newTournament.getStatus();
-		User host = newTournament.getHost();
-		
-		//TODO check proper order of columns and add to insert statement
-		String sql = "insert into tournaments "
+		int host_id = newTournament.getHost_id();
+		String date = newTournament.getDate();
+		String time = newTournament.getTime();
+		String sql = "insert into tournaments (tournament_name, host_id, status, tournament_date, tournament_time) values (?,?,?,?,?)";
+		template.update(sql, name, status, host_id, date, time);
 	}
 
 	@Override
 	public void editTournament(Tournament tournamentToEdit) {
-		// TODO Auto-generated method stub
-
+		String sql = "update tournaments set tournament_name = ?, host_id = ?, status = ?, tournament_date = ?, tournament_time = ? where tournament_id = ?";
+		template.update(sql, tournamentToEdit.getName(),tournamentToEdit.getHost_id(), tournamentToEdit.getName(), tournamentToEdit.getStatus(), tournamentToEdit.getDate(), tournamentToEdit.getTime());
 	}
 
 	@Override
 	public void cancelTournament(int id) {
-		// TODO Auto-generated method stub
+		String sql = "DELETE from tournaments WHERE tournament_id = ?";
+		template.update(sql, id);
 
 	}
 
--- java/src/main/java/com/techelevator/model/Tournament.java
+++ java/src/main/java/com/techelevator/model/Tournament.java
@@ -2,48 +2,69 @@ package com.techelevator.model;
 
 public class Tournament {
 	private String name;
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+	private String date;
+	private String time;
 	private int id;
-	private User[] listOfUsers;
-	private User host;
+	//TODO remove?
+	//private User[] listOfUsers;
+	private int host_id;
 	private String status;
 	
-	public Tournament(int id, User[] listOfUsers, User host, String status) {
+
+	
+	
+
+	
+	public Tournament(String name, String date, String time, int id, int host_id, String status) {
+		super();
+		this.name = name;
+		this.date = date;
+		this.time = time;
 		this.id = id;
-		this.listOfUsers = listOfUsers;
-		this.host = host;
+		this.host_id = host_id;
 		this.status = status;
 	}
 
+
+	public int getHost_id() {
+		return host_id;
+	}
+
+
+	public void setHost_id(int host_id) {
+		this.host_id = host_id;
+	}
+
+
+	public String getTime() {
+		return time;
+	}
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public User[] getListOfUsers() {
-		return listOfUsers;
-	}
-
-	public void setListOfUsers(User[] listOfUsers) {
-		this.listOfUsers = listOfUsers;
-	}
-
-	public User getHost() {
-		return host;
-	}
-
-	public void setHost(User host) {
-		this.host = host;
 	}
 
 	public String getStatus() {
