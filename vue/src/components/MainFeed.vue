036d6ccc55be33005e66664f6886869bfc5f1992
jerome zerovnik <zerovnik.jerome@gmail.com>
2021-02-03 12:55:11 -0500

    resolve merge

--- vue/src/components/MainFeed.vue
+++ vue/src/components/MainFeed.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
-    <div class="search">
-      <input type="text" placeholder="Search Tournaments">
+  <div id="main-container1">
+    <div id="item-wraper">
+    <div>
+      <input class="search" type="text" placeholder="Search Tournaments">
     </div>
     <ul class="tournaments">
       <li v-for="tournament in tournaments" v-bind:key="tournament.id">
-        <router-link v-bind:to="{ name: 'tournament-details', params: {id: tournament.id} }"> 
+        <router-link class="link-text" v-bind:to="{ name: 'tournament-details', params: {id: tournament.id} }"> 
           {{tournament.name}}
         </router-link>
       </li>
     </ul>
+  </div>
   </div>
 </template>
 
@@ -36,28 +38,71 @@ export default {
 </script>
 
 <style>
+#main-container1{
+width:100%;  
+}
+
+#item-wraper{
+width: 600px;
+justify-content: center;
+padding-left:2%;
+margin-left:auto;
+margin-right:auto
+}
+
 .search {
   margin:auto;
-  max-width:300px
+  width:100%;
+  min-height:50px;
+  border-radius:10px;
+  border-width:1px;
+  border-color:#ccc;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2em;
 }
 .tournaments {
   counter-reset: gradient-counter;
   list-style: none;
   margin: 1.75rem auto;
-  padding-left: 1rem;
+  padding-left: 0rem;
 }
 .tournaments li {
     background: white;
-    border-radius: 0 0.5rem 0.5rem 0.5rem;
+    border-radius: 0 0.5rem 0.5rem 0.0rem;
     counter-increment: gradient-counter;
-    margin-top: 1rem;
+    margin-top: 0rem;
     min-height: 3rem;
-    padding: 1rem 1rem 1rem 3rem;
+    padding: 1rem 1rem 1rem 0rem;
     position: relative;
-    max-width:50%;
+    max-width:100%;
+    border-top: solid darkgray 1px; 
+    text-align: center;   
+}
+  .link-text{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    color:black;
+    text-decoration: none;
     
+  } 
+
+  a{
+    color:black;
+    text-decoration:none;
+  }
+  a:hover{
+    color:#ccc;
+  }
+
+  input {
     
-    
+    border: 0px solid;
+}
+
+input:focus {
+    outline: none;
+    border-color: #9ecaed;
+    box-shadow: 0 0 10px #9ecaed;
 }
 
 
---
+++ vue/src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+computed:{
+  filteredList(){
+    let filteredUsers = this.users;
+      if (this.filter.firstName != "") {
+        filteredUsers = filteredUsers.filter((user) =>
+          user.firstName
+            .toLowerCase()
+            .includes(this.filter.firstName.toLowerCase())
+        );
+  }
+}
+
+}
+</script>
+
+<style>
+
+</style>
