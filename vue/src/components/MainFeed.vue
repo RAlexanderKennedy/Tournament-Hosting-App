fac8b5620281a4b8d0b99d3e74ffb5184f57d196
Blake Washington <blakewash1@gmail.com>
2021-02-03 11:41:26 -0500

    added seearch bar and some styling

--- vue/src/components/MainFeed.vue
+++ vue/src/components/MainFeed.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <div v-for="tournament in tournaments" v-bind:key="tournament.id">
-      <router-link v-bind:to="{ name: 'tournament-details', params: {id: tournament.id} }"> 
-        {{tournament.name}}
-      </router-link>
+    <div class="search">
+      <input type="text" placeholder="Search Tournaments">
     </div>
+    <ul class="tournaments">
+      <li v-for="tournament in tournaments" v-bind:key="tournament.id">
+        <router-link v-bind:to="{ name: 'tournament-details', params: {id: tournament.id} }"> 
+          {{tournament.name}}
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -31,5 +36,29 @@ export default {
 </script>
 
 <style>
+.search {
+  margin:auto;
+  max-width:300px
+}
+.tournaments {
+  counter-reset: gradient-counter;
+  list-style: none;
+  margin: 1.75rem auto;
+  padding-left: 1rem;
+}
+.tournaments li {
+    background: white;
+    border-radius: 0 0.5rem 0.5rem 0.5rem;
+    counter-increment: gradient-counter;
+    margin-top: 1rem;
+    min-height: 3rem;
+    padding: 1rem 1rem 1rem 3rem;
+    position: relative;
+    max-width:50%;
+    
+    
+    
+}
+
 
 </style>
